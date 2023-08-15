3240bf9b89cd385cea1e8ea4b9e442d8e8f31f6d
Phylloxera <adickey7@hotmail.com>
2023-08-15 05:17:44 -0500

    Update demo.r
    15aug2023. redefine variables and use [[]] notation

--- demo/demo.r
+++ demo/demo.r
@@ -11,42 +11,41 @@ fp <- "GeoSpatial\\Urban"; file <- "tl_2020_us_uac20"; cdist <- 100
   i <- 1; ua$dist <- double(nrow(ua)); ua$id2 <- character(nrow(ua))
   dist_df <- head(ua[, c(1, 14, 15)], n = 0)[, 1:3]; agglom_list <- list()
   runtimes <- data.frame(iter = character(), runtime = double())
-  nn_df <- data.frame(nn_UACE20 = character(), dist = double(),
-                      UACE20 = character())}
+  nn_df <- data.frame(nn_UACE = character(), dist = double(),
+                      UACE = character())}
 
 #loop code block (runtime inverse proportional to cdist, ~19h on laptop)
-for (iter in 1:nrow(ua)) {if (ua$UACE20[1] %in% dist_df[, 3]) {break}
-ua$dist <- as.vector(st_distance(ua[ua$UACE20 == ua$UACE20[1], ], ua))
-ua$id2 <- ua$UACE20[1]; distrows1 <- nrow(dist_df)
+for (iter in 1:nrow(ua)) {if (ua[[1]][1] %in% dist_df[, 3]) {break}
+ua$dist <- as.vector(st_distance(ua[ua[[1]] == ua[[1]][1], ], ua))
+ua$id2 <- ua[[1]][1]; distrows1 <- nrow(dist_df)
 dist_df <- rbind(data.frame(ua[ua$dist <= cdist, c(1, 14, 15)])[, 1:3], dist_df)
 distrows2 <- nrow(dist_df)
 nn <- data.frame(ua[ua$dist > cdist, c(1, 14, 15)])[1:3]
-nn <- nn[which.min(nn$dist), ]; names(nn) <- c("nn_UACE20","dist","UACE20")
+nn <- nn[which.min(nn$dist), ]; names(nn) <- c("nn_UACE","dist","UACE")
 distrows <- distrows2 - distrows1
-if (distrows > 1) {ids2check <- setdiff(dist_df$UACE20[1:distrows],
-                                        ua$UACE20[1])
-union_vector <- c(ua$UACE20[1], ids2check[1])
+if (distrows > 1) {ids2check <- setdiff(dist_df[[1]][1:distrows], ua[[1]][1])
+union_vector <- c(ua[[1]][1], ids2check[1])
 union_vector <- union_vector[!is.na(union_vector)]
 while(length(ids2check) > 0) {
-  ua$dist <- as.vector(st_distance(ua[ua$UACE20 == ids2check[1], ], ua))
+  ua$dist <- as.vector(st_distance(ua[ua[[1]] == ids2check[1], ], ua))
   ua$id2 <- ids2check[1]
   dist_df <- rbind(data.frame(ua[ua$dist <= cdist, c(1, 14, 15)])[, 1:3],
                    dist_df)
   nn2 <- data.frame(ua[ua$dist > cdist, c(1, 14, 15)])[1:3]
-  nn2 <- nn2[which(!(nn2$UACE20 %in% union_vector)), ]
+  nn2 <- nn2[which(!(nn2[[3]] %in% union_vector)), ]
   nn2 <- nn2[which.min(nn2$dist), ]
-  names(nn2) <- c("nn_UACE20", "dist", "UACE20"); nn <- rbind(nn, nn2)
-  nn <- nn[which(!(nn$nn_UACE20 %in% union_vector)), ]
+  names(nn2) <- c("nn_UACE", "dist", "UACE"); nn <- rbind(nn, nn2)
+  nn <- nn[which(!(nn[[1]] %in% union_vector)), ]
   distrows2 <- nrow(dist_df); distrows <- distrows2 - distrows1
-  ids2check <- unique(setdiff(dist_df$UACE20[1:distrows], union_vector))
+  ids2check <- unique(setdiff(dist_df[[1]][1:distrows], union_vector))
   union_vector <- c(union_vector, ids2check[1])
   union_vector <- union_vector[!is.na(union_vector)]}
-u <- st_sf(st_union(ua[ua$UACE20 %in% union_vector, ]))
-nn <- nn[which(!(nn$nn_UACE20 %in% union_vector)), ]
+u <- st_sf(st_union(ua[ua[[1]] %in% union_vector, ]))
+nn <- nn[which(!(nn[[1]] %in% union_vector)), ]
 nn <- nn[which.min(nn$dist), ]} else {
-  u <- st_sf(st_union(ua[ua$UACE20 == ua$UACE20[1], ]))}
+  u <- st_sf(st_union(ua[ua[[1]] == ua[[1]][1], ]))}
 if (length(union_vector) == 0) {ua <- ua[c(2:nrow(ua), 1), ]} else {
-  sf_remain <- ua[!(ua$UACE20 %in% union_vector), ]; df <- head(ua, n = 0)
+  sf_remain <- ua[!(ua[[1]] %in% union_vector), ]; df <- head(ua, n = 0)
   df[1, ] <- c(paste0("agglom", i), character(1L), character(1L), character(1L),
                character(1L), character(1L), character(1L), character(1L),
                double(1L), double(1L), character(1L), character(1L), u,
@@ -57,7 +56,7 @@ if (length(union_vector) == 0) {ua <- ua[c(2:nrow(ua), 1), ]} else {
   i <- i + 1}; nn_df <- rbind(nn_df, nn)}
 
 #clean up code block
-{dist_df <- dist_df[which(dist_df$UACE20 != dist_df$id2), ]
+{dist_df <- dist_df[which(dist_df[[1]] != dist_df$id2), ]
 remove(df, nn, nn2, sf_remain, u, d1, d2, distrows, distrows1, distrows2, file,
        i, ids2check, iter, union_vector)}
 
@@ -67,6 +66,9 @@ file <- "\\2020_Census_ua_list_all.xlsx"; library(readxl)
 #process agglom_list to metadata. results in a table of urban agglomerations
 #corresponding to the user provided distance
 {ua_meta <- read_excel(paste0(fp, file))
+ if ("LSADC" %in% names(ua_meta)) {
+    ua_meta2 <- ua_meta[, -which(names(ua_meta) == "LSADC")]
+    ua_meta <- ua_meta2; remove(ua_meta2)}
 for (i in 1:length(agglom_list)) {
   df <- ua_meta[ua_meta[[1]] %in% agglom_list[[i]], ]
   df <- df[order(df[[3]]), ]
@@ -81,7 +83,7 @@ for (i in 1:length(agglom_list)) {
 ua_meta_ss <- ua_meta[ua_meta[[1]] %in% ua[[1]], ]
 ua_meta_ss$EABCOMBINED <- NA; ua_meta_ss$UACES <- NA
 agglom_meta <- rbind(agglom_meta, ua_meta_ss)
-agglom_meta[[9]] <- agglom_meta[[3]]/agglom_meta[[6]]
+agglom_meta[["POPDEN"]] <- agglom_meta[["POP"]]/agglom_meta[["AREALANDSQMI"]]
 agglom_meta$AREALANDSQMI <- round(agglom_meta$AREALANDSQMI, 0)
 agglom_meta$AREAWATERSQMI <- round(agglom_meta$AREAWATERSQMI, 0)
 agglom_meta$POPDEN <- round(agglom_meta$POPDEN, 0); nn_df2 <- nn_df
